8c5a61ac09b2d027ba00620ddd520b763bef22fe
kajal-2011 <kajalrekha888@gmail.com>
2023-05-31 03:12:09 +0600

    hero section added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,65 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import { Navigation, Pagination, Autoplay, Parallax } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { data } from "@/data/sliderImages";
+import Image from "next/image";
+import Link from "next/link";
+import Overlay from "./Overlay";
+
 const Hero = () => {
   return (
-    <section>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+    <section className="w-full h-[70vh]">
+      <Swiper
+        parallax={true}
+        grabCursor={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        speed={700}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation, Pagination, Autoplay, Parallax]}
+        className="mySwiper w-full h-full"
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <div className="w-full h-full relative">
+              <Image
+                src={item.src}
+                alt={item.alt}
+                height={1920}
+                width={1000}
+                priority
+                className="w-full h-full object-cover"
+              />
+              <Overlay />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center z-[2] space-y-5">
+                <h1
+                  data-swiper-parallax="-300%"
+                  className="text-8xl font-semibold"
+                >
+                  {item.headline}
+                </h1>
+                <p data-swiper-parallax="-500%" className="text-lg">
+                  {item.alt}
+                </p>
+                <div data-swiper-parallax="-600%">
+                  {" "}
+                  <Link href="/products" className="btn btn-accent">
+                    {item.cta}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
---
+++ data/sliderImages.ts
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: 1,
+    src: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1685122322/pizza-margherita_lq9gjs.jpg",
+    alt: "Savor the Perfect Slice!",
+    headline: "Savor the Perfect Slice!",
+    paragraph:
+      "Pizza is a delicious Italian dish consisting of a round, flat base topped with various ingredients such as cheese, tomato sauce, and toppings like pepperoni, mushrooms, or vegetables. It is typically baked in an oven until the cheese melts and the crust becomes crispy. Pizza is loved by people of all ages and is a popular choice for gatherings and parties.",
+    cta: "Order Now",
+  },
+  {
+    id: 2,
+    src: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1685122307/Smashburger-recipe-120219_oaokje.webp",
+    alt: "burger images",
+    headline: "Bite into Burger Bliss!",
+    paragraph:
+      "Burgers are a classic American food loved by many. They are sandwiches made with a patty of ground meat, such as beef or chicken, placed inside a sliced bun. Burgers are often accompanied by condiments like lettuce, tomatoes, pickles, and various sauces. They can be customized with additional toppings like cheese, bacon, or fried onions to enhance the flavor. Burgers are a convenient and satisfying option for a quick meal on the go.",
+    cta: "Order Now",
+  },
+  {
+    id: 3,
+    src: "https://res.cloudinary.com/dh0ado2ir/image/upload/v1685122335/CHE03_PLANT_BASED_CREAMY_PASTA_ALFREDO_ts6cro.jpg",
+    alt: "best pasta",
+    headline: "Indulge in Italian Delights!",
+    paragraph:
+      "Pasta is a versatile and comforting Italian dish made from unleavened dough, consisting of flour, eggs, and water. It comes in various shapes and sizes like spaghetti, penne, or fettuccine. Pasta is typically boiled until it becomes tender and then served with a variety of sauces like marinara, Alfredo, or pesto. It can be paired with meat, seafood, or vegetables to create a hearty and delicious meal that is loved by pasta enthusiasts around the world.",
+    cta: "Order Now",
+  },
+];
